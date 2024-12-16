2221705ea324135d191814c66a3849e141c4dcee
OtokarSefer <Otokar.Sefer@voco.ee>
2024-12-16 11:49:02 +0200

    im touching myself
    Co-authored-by: endtrastic <endtrastic@users.noreply.github.com>

---
+++ Tinder-tuah/src/components/list.jsx
@@ -0,0 +1,38 @@
+const men = [
+    {  
+        id: 1,
+        Name: "Alpha male",
+        Age: "12",
+        description: 'ready & single',
+        img: "img/men/man.jpeg"
+    },
+    {  
+        id: 2,
+        Name: "Alpha male",
+        Age: "12",
+        description: 'ready & single',
+        img: "img/men/man.jpeg"
+    },
+    {  
+        id: 3,
+        Name: "Alpha male",
+        Age: "12",
+        description: 'ready & single',
+        img: "img/men/man.jpeg"
+    },
+    {  
+        id: 4,
+        Name: "Alpha male",
+        Age: "12",
+        description: 'ready & single',
+        img: "img/men/man.jpeg"
+    },
+    {  
+        id: 5,
+        Name: "Alpha male",
+        Age: "12",
+        description: 'ready & single',
+        img: "img/men/man.jpeg"
+    },
+
+]
